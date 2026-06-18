43a280a4800cf0e5e334841d56b8249624fd0fd9
nakedmarketing <hello@nakedmarketing.co>
2026-06-18 14:46:52 +1200

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ import http from "http";
 
 const PORT = process.env.PORT || 3000;
 
-async function getIntegrations() {
+async function getConnections() {
   const response = await fetch(
-    "https://api.reportingninja.com/v1/integrations",
+    "https://api.reportingninja.com/v1/connections",
     {
       method: "POST",
       headers: {
         Authorization: `Bearer ${process.env.REPORTING_NINJA_API_KEY}`,
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({})
+      body: JSON.stringify({
+        integration_id: "google_ads"
+      })
     }
   );
 
@@ -20,7 +22,7 @@ async function getIntegrations() {
 
 const server = http.createServer(async (req, res) => {
   try {
-    const data = await getIntegrations();
+    const data = await getConnections();
 
     res.writeHead(200, {
       "Content-Type": "application/json"
